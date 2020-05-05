2d1e40c6c2b4efadafe8ccb123d1471b2c2bcda5
Djnsakn <504265797@qq.com>
2020-05-05 11:49:55 +0800

    add Ribbon

---
+++ eureka-client-consumer/src/main/java/com/cgf/eurekaclientconsumer/EurekaClientConsumerApplication.java
@@ -0,0 +1,31 @@
+package com.cgf.eurekaclientconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientConsumerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurekaClientConsumerApplication.class, args);
+    }
+
+    /**
+     * springcloud中提倡rest风格的微服务
+     * 向spring容器中注入RestTemplate
+     *
+     * 使用LoadBalanced表明这个restRemplate开启负载均衡的功能。
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ eureka-client-consumer/src/test/java/com/cgf/eurekaclientconsumer/EurekaClientConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgf.eurekaclientconsumer;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
