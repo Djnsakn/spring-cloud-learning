88475330b2f5c628f6540b3ba9d384ca8f5db12b
Djnsakn <504265797@qq.com>
2020-05-11 16:59:05 +0800

    add config

---
+++ config-client-autofresh/src/main/java/com/cgf/configclient/ConfigClientAutofreshApplication.java
@@ -0,0 +1,13 @@
+package com.cgf.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientAutofreshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientAutofreshApplication.class, args);
+    }
+
+}
---
+++ config-client/src/main/java/com/cgf/configclient/HelloController.java
@@ -0,0 +1,19 @@
+package com.cgf.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope  // 用于自动刷新配置
+public class HelloController {
+    //属性注入
+    @Value("${spring.application.name}")
+    String name;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return "hi "+name;
+    }
+}
